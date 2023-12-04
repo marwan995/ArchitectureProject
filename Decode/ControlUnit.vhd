d769b1abc38ea94371b1c3d0ad6eb697b67c0c11
marwan995 <mroansamy99@gamil.com>
2023-12-04 20:53:51 +0200

    control unit

--- Decode/ControlUnit.vhd
+++ Decode/ControlUnit.vhd
@@ -9,31 +9,30 @@ ENTITY ControlUnit IS
         instruction : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
 
         -- --alu 
-        -- aluEnable : OUT STD_LOGIC;
-        -- src1Selector : OUT STD_LOGIC;
-        -- src2Selector : OUT STD_LOGIC;
-        -- src3Selector : OUT STD_LOGIC;
-        -- aluOperationSelector : OUT STD_LOGIC;
+        aluEnable : OUT STD_LOGIC;
+        src1Selector : OUT STD_LOGIC;
+        src2Selector : OUT STD_LOGIC;
+        src3Selector : OUT STD_LOGIC;
+        aluOperationSelector : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
 
         -- memory
         memoryEnable : OUT STD_LOGIC;
-        -- memoryOrIO : OUT STD_LOGIC;
+        memoryOrIO : OUT STD_LOGIC;
         -- readOrWrite : OUT STD_LOGIC;
-        -- addressSelector : OUT STD_LOGIC;
-        -- memoryValueFlag : OUT STD_LOGIC;
-        -- pcUpdate : OUT STD_LOGIC;
-        -- spIncOrDec : OUT STD_LOGIC;
-        -- protectMemory : OUT STD_LOGIC;
-        -- jumpStopFlag : OUT STD_LOGIC;
+        addressSelector : OUT STD_LOGIC;
+        memoryValueFlag : OUT STD_LOGIC;
+        spEnable : OUT STD_LOGIC;
+        spIncOrDec : OUT STD_LOGIC;
+        protectMemory : OUT STD_LOGIC;
+        jumpStopFlag : OUT STD_LOGIC;
 
         --write back
         writeBackEnable : OUT STD_LOGIC;
         reg2Write : OUT STD_LOGIC;
-        writeBackSelector : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+        writeBackSelector : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
 
         --pc
-        -- jmpFlag : OUT STD_LOGIC;
-        -- pcMemoryFlag : OUT STD_LOGIC
+        jmpFlag : OUT STD_LOGIC
 
     );
 END ControlUnit;
@@ -41,18 +40,21 @@ END ControlUnit;
 ARCHITECTURE ArchControlUnit OF ControlUnit IS
 
     SIGNAL zeroOperand, oneOperand, twoOperand, threeOperand : STD_LOGIC;
+    SIGNAL pcTemp, memoryValueTemp : STD_LOGIC;
+    SIGNAL isJump : STD_LOGIC;
 
 BEGIN
-    -- initialize the operand
-    zeroOperand <= (instruction(15)) nor (instruction(14));
+    -------------------------------initialize the operand ----------------------------
+    zeroOperand <= (instruction(15)) NOR (instruction(14));
     oneOperand <= NOT((instruction(15))) AND instruction(14);
     twoOperand <= instruction(15) AND NOT(instruction(14));
     threeOperand <= (instruction(15)) AND instruction(14);
+    isJump <= (oneOperand AND (instruction(1) OR instruction(2)));
 
     -------------------------------Write Back ----------------------------
     writeBackEnable <=
         (oneOperand AND (instruction(1) NOR instruction(2))) OR
-        (twoOperand AND (instruction(2) nand instruction(1)))OR
+        (twoOperand AND (instruction(2) NAND instruction(1)))OR
         (threeOperand);
 
     reg2Write <= (twoOperand) AND ((instruction(1)) NOR (instruction(2)));
@@ -67,13 +69,53 @@ BEGIN
     writeBackSelector(1) <= (oneOperand AND (NOT instruction(6) AND instruction(5) AND NOT instruction(4) AND NOT instruction(3))) OR
     (oneOperand AND (instruction(6) AND NOT instruction(5) AND NOT instruction(4) AND instruction(3))) OR
     (oneOperand AND (instruction(6)AND instruction(5) AND instruction(4) AND instruction(3)) AND instruction(0));
-    -------------------------------memory ----------------------------
+    -------------------------------Memory ----------------------------
     memoryEnable <=
         (zeroOperand AND instruction(6))OR
         (oneOperand AND (instruction(6) AND instruction(5)))OR
         (oneOperand AND (instruction(6) XOR instruction(5)) AND (instruction(3) NOR instruction(4)))OR
         (oneOperand AND (instruction(1) AND instruction(2)))
-
         ;
+    memoryOrIO <=
+        (oneOperand AND (instruction(3) NOR instruction(4)) AND (instruction(5) XOR instruction(6)));
+    addressSelector <=
+        (oneOperand AND (instruction(6)AND instruction(5) AND NOT (instruction(4))));
+
+    memoryValueTemp <= (NOT instruction(15) AND (instruction(1) AND instruction(2)));
+    memoryValueFlag <= memoryValueTemp;
+    pcTemp <= (zeroOperand AND instruction(4));
+    jmpFlag <= (memoryValueTemp OR pcTemp);
+    spEnable <= pcTemp OR memoryValueTemp OR (oneOperand AND(instruction(4)AND instruction(5) AND instruction(6))AND NOT(instruction(0)));
+    spIncOrDec <= pcTemp OR (oneOperand AND(NOT (instruction(0)) AND instruction(3) AND instruction(4) AND instruction(5) AND instruction(6)));
+    protectMemory <= (oneOperand AND (instruction(6)AND instruction(5) AND NOT (instruction(4))));
+    jumpStopFlag <= ((zeroOperand AND instruction(2)) OR (memoryValueTemp));
+    -------------------------------Alu ----------------------------
+    aluEnable <= ((oneOperand AND NOT(instruction(6)) AND (instruction(1) NOR instruction(2))) OR
+        (threeOperand)OR
+        (twoOperand AND instruction(1)));
+    src1Selector <= (threeOperand) OR (twoOperand AND instruction(0));
+    src2Selector <= (twoOperand OR oneOperand) AND(instruction(0) AND NOT(instruction(6)));
+    src3Selector <= threeOperand;
+    aluOperationSelector(3) <= (threeOperand)OR
+    (twoOperand AND instruction(1));
+
+    aluOperationSelector(2) <= ((threeOperand AND instruction(2)) OR
+    (oneOperand AND ((instruction(5) AND (instruction(4)OR instruction(3))) OR (NOT instruction(5) AND (instruction(4)AND instruction(3)))))
+    );
+
+    aluOperationSelector(1) <= ((threeOperand AND instruction(4)) OR
+    (oneOperand and (NOT instruction(6) AND(instruction(5)AND instruction(4)))
+    )or
+    (oneOperand and (NOT instruction(6)  and NOT instruction(5) and ( instruction(4) xor instruction(3) ) ))
+    );
+
+    aluOperationSelector(0) <= ((threeOperand AND (instruction(1) nand instruction(3))) OR
+    (twoOperand AND instruction(0))OR
+    (oneOperand AND instruction(0) AND instruction(3))OR
+    (oneOperand AND ( not instruction(0) and not instruction(6) and not instruction(3)))
+
+    );
+    -------------------------------PC ----------------------------
+    jmpFlag <= oneOperand and (instruction(1) xor instruction(2));
 
 END ARCHITECTURE;
--- Decode/x.py
+++ Decode/x.py
@@ -39,3 +39,85 @@ input_strings = ["0000000000000000",
                 ]
 resulting_string = create_force_commands(input_strings)
 print(resulting_string)
+# vcom -work work -2002 -explicit -O0 {D:/3rd-cmp/First semster/Arch/project/Decode/ControlUnit.vhd}
+# # Model Technology ModelSim ALTERA vcom 10.1d Compiler 2012.11 Nov  2 2012
+# # -- Loading package STANDARD
+# # -- Loading package TEXTIO
+# # -- Loading package std_logic_1164
+# # -- Loading package NUMERIC_STD
+# # -- Compiling entity ControlUnit
+# # -- Compiling architecture ArchControlUnit of ControlUnit
+# vsim -voptargs=+acc work.controlunit
+# # vsim -voptargs=+acc work.controlunit 
+# # Loading std.standard
+# # Loading std.textio(body)
+# # Loading ieee.std_logic_1164(body)
+# # Loading ieee.numeric_std(body)
+# # Loading work.controlunit(archcontrolunit)
+# add wave -position insertpoint  \
+# sim:/controlunit/instruction \
+# sim:/controlunit/src2Selector \
+# sim:/controlunit/zeroOperand \
+# sim:/controlunit/oneOperand \
+# sim:/controlunit/twoOperand \
+# sim:/controlunit/threeOperand
+# force -freeze sim:/controlunit/instruction 0000000000000000 0
+# run
+# force -freeze sim:/controlunit/instruction 0000000001111010 0
+# run
+# force -freeze sim:/controlunit/instruction 0000000001111100 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000000000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000001000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000010000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000011000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000100000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000101001 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000110001 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000111001 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001000000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001001001 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001110000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001111000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001100000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001101000 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001111001 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000001110001 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000001010 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000001100 0
+# run
+# force -freeze sim:/controlunit/instruction 0100000000010110 0
+# run
+# force -freeze sim:/controlunit/instruction 1000000000000000 0
+# run
+# force -freeze sim:/controlunit/instruction 1000000000011110 0
+# run
+# force -freeze sim:/controlunit/instruction 1000000000000011 0
+# run
+# force -freeze sim:/controlunit/instruction 1100000000000010 0
+# run
+# force -freeze sim:/controlunit/instruction 1100000000011010 0
+# run
+# force -freeze sim:/controlunit/instruction 1100000000001100 0
+# run
+# force -freeze sim:/controlunit/instruction 1100000000001110 0
+# run
+# force -freeze sim:/controlunit/instruction 1100000000010000 0
+# run

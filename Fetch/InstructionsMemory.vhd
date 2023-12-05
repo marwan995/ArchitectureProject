bb960d833af04a0a7dc2fc6becb796d63e6f5508
marwan995 <mroansamy99@gamil.com>
2023-12-05 16:15:15 +0200

    Fetch

--- Fetch/InstructionsMemory.vhd
+++ Fetch/InstructionsMemory.vhd
@@ -1,6 +1,7 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.ALL;
+
 ENTITY instructionMemory IS
     PORT (
         clk : IN STD_LOGIC;
@@ -10,9 +11,15 @@ ENTITY instructionMemory IS
         dataout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
 END ENTITY instructionMemory;
 
-ARCHITECTURE Arch_instructionMemory OF instructionMemory IS TYPE instructionMemory_type
-    IS ARRAY(0 TO 4096) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
-    SIGNAL instructionMemory : instructionMemory_type;
+ARCHITECTURE Arch_instructionMemory OF instructionMemory IS 
+    TYPE instructionMemory_type IS ARRAY(0 TO 4096) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL instructionMemory : instructionMemory_type := (
+        "0100000001000000",
+        "0100000001100000",
+        "0100000001111001",
+        "1100000000001110",
+        others => (others => '0')
+    );
 BEGIN
     PROCESS (clk) IS
     BEGIN
